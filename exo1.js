dabe2f16f6bfbc2eb3bfa0d0cc67b2f07663fa7d
EcharpeBleue <zawiczrczarny@gmail.com>
2024-01-24 14:13:49 +0100

    Initial commit

---
+++ exo1.js
@@ -0,0 +1,6 @@
+let texte = '';
+
+for (let i = 0; i < 17; i++) {
+    texte = Math.pow(2,i);
+    console.log(`La puissance de 2 par ${i} est ${texte}.`);
+}
---
+++ fonction1.js
@@ -0,0 +1,46 @@
+function afficherLesNombres(nombre)
+{
+    for (let i=1;i<=nombre;i++){
+        console.log(i);
+    }
+}
+
+function afficherLesNombresRetour(nombre){
+    for(let j=nombre;j>0;j--){
+        console.log(j);
+    }
+}
+
+function afficherNombresPairs(nombre){
+    for(let u=1;u<=nombre;u++){
+        let pair = u*2;
+        texte += `${pair} - `;
+    }
+    console.log(texte);
+}
+
+function afficherPuissancesDeDeux(){
+    for(let o=1;o<17;o++){
+        let puissanceResultat = Math.pow(2,o);
+        texte+=`La puissance de 2 par ${o} donne ${puissanceResultat}.\n`;
+    }
+    console.log(texte);
+}
+
+function afficherPuissancesDeDeuxWhile(){
+    let k=1;
+    while(k<17){
+         let puissanceResultat = Math.pow(2,k);
+         texte+=`La puissance de 2 par ${k} donne ${puissanceResultat}.\n`;
+         k++;
+    }
+    console.log(texte);
+}
+
+let texte="";
+let n = prompt(`Saisir un nombre`);
+afficherLesNombres(n);
+afficherLesNombresRetour(n);
+afficherNombresPairs(n);
+afficherPuissancesDeDeux();
+afficherPuissancesDeDeuxWhile();
